baa930773b10133ed61864c014ca3c0886ece29d
enma <takanamoo@gmail.com>
2018-11-30 07:18:03 +0000

    try to stack Cube

--- my-app/src/Tile.elm
+++ my-app/src/Tile.elm
@@ -1,4 +1,4 @@
-module Tile exposing (Array2D, fill, foldl, get, h, indexedMap, leftSide, map, offsetX, offsetY, quarterX, quarterY, rightSide, set, toList, top, w)
+module Tile exposing (h, leftSide, offsetX, offsetY, quarterX, quarterY, rightSide, top, w)
 
 import Array exposing (..)
 import Browser
@@ -19,68 +19,22 @@ import TypedSvg.Types exposing (..)
 -- Make Tile
 
 
-type alias Array2D a =
-    Array (Array a)
-
-
-fill : Int -> Int -> a -> Array2D a
-fill rowLength colLength data =
-    Array.repeat rowLength (Array.repeat colLength data)
-
-
-get : Int -> Int -> Array2D a -> Maybe a
-get rowIndex colIndex rows =
-    Array.get rowIndex rows
-        |> andThen
-            (\row ->
-                Array.get colIndex row
-            )
-
-
-set : Int -> Int -> a -> Array2D a -> Array2D a
-set rowIndex colIndex data rows =
-    case Array.get rowIndex rows of
-        Just row ->
-            Array.set rowIndex (Array.set colIndex data row) rows
-
-        Nothing ->
-            rows
-
-
-map : (a -> b) -> Array2D a -> Array2D b
-map f rows =
-    Array.map (Array.map f) rows
-
-
-indexedMap : (Int -> Int -> a -> b) -> Array2D a -> Array2D b
-indexedMap f rows =
-    Array.indexedMap (Array.indexedMap << f) rows
-
-
-foldl : (a -> b -> b) -> b -> Array2D a -> b
-foldl f init rows =
-    Array.foldl (\row memo -> Array.foldl f memo row) init rows
-
-
-toList : Array2D a -> List (List a)
-toList rows =
-    Array.toList (Array.map Array.toList rows)
 
 
 h =
-    32
+    44
 
 
 w =
-    32
+    44
 
 
 offsetX =
-    100
+      -50
 
 
 offsetY =
-    100
+    150
 
 
 quarterX : Float -> Float -> Float
@@ -106,3 +60,5 @@ leftSide =
 rightSide : List ( Float, Float )
 rightSide =
     [ ( w/2, h/2 ), ( w, h/4 ), ( w, 3*h/4 ), ( w/2, h ) ]
+        
+        
